5beb57b1b42e2b8b043bd369c26819baf45703f8
Ahmed Tammaa <ahmedtammaa101@hotmail.com>
2022-04-29 10:39:49 +0200

    Delete run_script.bat

--- run_script.bat
+++
@@ -1,4 +0,0 @@
-docker build -t test .
-docker run --rm --mount type=bind,source="C:\Users\Tammaa\Machine Translation\Machine_Translation",target=/docker -p 8888:8888 --gpus all test
-
-sleep (10000)
